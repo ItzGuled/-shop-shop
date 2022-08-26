32fdb3a3027d3c6da02d3b622f641970c6b97699
itzguled <itzguled55337@gmail.com>
2022-08-25 21:09:54 -0500

    indexdDB shopping cart

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import Auth from '../../utils/auth';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Auth from "../../utils/auth";
+import { Link } from "react-router-dom";
 
 function Nav() {
+
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
         <ul className="flex-row">
           <li className="mx-1">
-            <Link to="/orderHistory">Order History</Link>
+            <Link to="/orderHistory">
+              Order History
+            </Link>
           </li>
           <li className="mx-1">
             {/* this is not using the Link component to logout or user and then refresh the application to the start */}
@@ -22,10 +25,14 @@ function Nav() {
       return (
         <ul className="flex-row">
           <li className="mx-1">
-            <Link to="/signup">Signup</Link>
+            <Link to="/signup">
+              Signup
+            </Link>
           </li>
           <li className="mx-1">
-            <Link to="/login">Login</Link>
+            <Link to="/login">
+              Login
+            </Link>
           </li>
         </ul>
       );
@@ -35,13 +42,15 @@ function Nav() {
   return (
     <header className="flex-row px-1">
       <h1>
-        <Link to="/">
+      <Link to="/">
           <span role="img" aria-label="shopping bag">🛒</span>
           Shop Buddy
         </Link>
       </h1>
 
-      <nav>{showNavigation()}</nav>
+      <nav>
+        {showNavigation()}
+      </nav>
     </header>
   );
 }
